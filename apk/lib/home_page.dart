7f3257e0a19ce6cd8f63a7b5305ab64d7c7624ea
chandan <chandankumar@gmail.com>
2021-02-08 20:47:52 +0530

    day 2 over

---
+++ apk/lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("catalog App")),
+      body: Center(
+        child: Container(
+          child: Text("thanks r "),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
