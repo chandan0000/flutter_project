dd3c8e62af624befd0cc109ba831b9c3a82c3e81
chandan <chandankumar@gmail.com>
2021-02-08 11:09:16 +0530

    my project

---
+++ apk/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Container(
+          child: Text("welcome two 20 days"),
+        ),
+      ),
+    );
+  }
+}
