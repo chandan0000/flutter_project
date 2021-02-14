daf971335abcd6a6dd7a947e6e6ab29b0b1771b9
chandan <chandankumar@gmail.com>
2021-02-14 09:33:25 +0530

    theme xtraxt

--- apk/lib/main.dart
+++ apk/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:apk/pages/home_page.dart';
 import 'package:apk/utils/routes.dart';
+import 'package:apk/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+//import 'wigets/themes.dart';
 
 import 'pages/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 //import 'package:google';
 void main() {
@@ -16,13 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          primaryTextTheme: GoogleFonts.latoTextTheme()),
-      darkTheme: ThemeData(
-        brightness: Brightness.light,
-      ),
+      theme: MyTheme.lightTheme(context),
+
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       // home:  HomePage(),
--- apk/lib/pages/home_page.dart
+++ apk/lib/pages/home_page.dart
@@ -5,7 +5,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("catalog App")),
+      appBar: AppBar(
+        // backgroundColor: Colors.white,
+        // elevation: 0.0,
+        // iconTheme: IconThemeData(color: Colors.black),
+        title: Text("catalog App"
+            // style: TextStyle(color: Colors.black),
+
+            ),
+      ),
       body: Center(
         child: Container(
           child: Text("hi mr hacker "),
